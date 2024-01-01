bf4f2f67ac9ac4258f1f854dadf75872f9ff2f73
Danil-Kolmahin <danil.kolmahin@gmail.com>
2024-01-01 14:21:36 +0000

    Add auto deploy to handler

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -5,14 +5,13 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @Get('version')
+  version() {
+    return process.env.npm_package_version;
+  }
 
   @Get()
   getData() {
     return this.appService.getData();
-  }
-
-  @Get('version')
-  version() {
-    return process.env.npm_package_version;
   }
 }
--- apps/handler/src/app/app.controller.ts
+++ apps/handler/src/app/app.controller.ts
@@ -5,6 +5,10 @@ import { AppService } from './app.service';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @Get('version')
+  version() {
+    return process.env.npm_package_version;
+  }
 
   @Get()
   getData() {
